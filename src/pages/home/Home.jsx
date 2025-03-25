fc4f85b54614c5fb52fca324f56a4914238fdad1
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-25 08:58:37 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,44 +1,41 @@
+import { faker } from '@faker-js/faker';
 import React from 'react'
-import { useState } from 'react';
-
-
-
+import { useState } from 'react'
 
 export default function Home() {
-  const mi = [11, 22, 33];
-  const [a, b] = mi;
-
-
-
-
-  const [count, setCount] = useState(1);
-
-
-
-  const handleCount = () => {
-
-    setCount((prev) => {
-      return 9;
-    });
-    //  console.log(count);
+  const [users, setUsers] = useState([]);
+  const handleUser = () => {
+    const user = {
+      id: faker.string.ulid(),
+      image: faker.image.avatarGitHub(),
+      username: faker.internet.displayName(),
+      email: faker.internet.email(),
+      reaction: faker.internet.emoji()
+    };
+    setUsers((prev) => [...prev, user]);
 
   }
 
-  console.log('render');
-  console.log(count);
-
 
   return (
-    <div className='p-4'>
+    <div className='p-5'>
 
+      <button
+        onClick={handleUser}
+        className='bg-black text-white px-5 py-1 cursor-pointer'>Add User</button>
 
-      <h1 className='text-2xl'>{count}</h1>
+      {users.map((user) => {
+        return <div key={user.id} className='space-y-2 flex gap-7 items-start'>
+          <div>
+            <h2>{user.username}</h2>
+            <img src={user.image} className='h-[100px]' alt="" />
 
+          </div>
 
-      <button onClick={handleCount} className='bg-black text-white px-2 py-1 cursor-pointer'>Increment</button>
+          <button className='bg-pink-500 text-white px-2'>Remove</button>
 
-
-
+        </div>
+      })}
 
     </div>
   )
