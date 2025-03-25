6a01f3f5d6c1dc311a7513541ca2afc74f2db6d5
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-25 09:07:42 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,24 +7,31 @@ export default function Home() {
   const handleUser = () => {
     const user = {
       id: faker.string.ulid(),
-      image: faker.image.avatarGitHub(),
+      image: faker.image.avatar(),
       username: faker.internet.displayName(),
       email: faker.internet.email(),
       reaction: faker.internet.emoji()
     };
     setUsers((prev) => [...prev, user]);
+  }
+  const handleRemove = (index) => {
+    // const numbers = [11, 22, 33, 44, 55];
+    // numbers.splice(1, 1)
 
+    // console.log(numbers);
+    users.splice(index, 1);
+
+    setUsers((prev) => [...prev]);
   }
 
 
   return (
     <div className='p-5'>
-
       <button
         onClick={handleUser}
         className='bg-black text-white px-5 py-1 cursor-pointer'>Add User</button>
 
-      {users.map((user) => {
+      {users.map((user, i) => {
         return <div key={user.id} className='space-y-2 flex gap-7 items-start'>
           <div>
             <h2>{user.username}</h2>
@@ -32,7 +39,9 @@ export default function Home() {
 
           </div>
 
-          <button className='bg-pink-500 text-white px-2'>Remove</button>
+          <button
+            onClick={() => handleRemove(i)}
+            className='bg-pink-500 text-white px-2'>Remove</button>
 
         </div>
       })}
