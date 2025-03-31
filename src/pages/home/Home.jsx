982e86f78ddc8f00bfff1920f529586585291ae0
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-31 09:34:16 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,67 +1,133 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
 
+import axios from 'axios'
+import React, { useEffect, useState, useTransition } from 'react'
 
 export default function Home() {
 
-  const nav = useNavigate();
-
   const [data, setData] = useState();
-  const [load, setLoad] = useState(false);
+  const [err, setError] = useState();
 
-  const getData = async () => {
-    setLoad(true); /// calll
+
+  const [pending, setTransition] = useTransition();
+
+  const getTodos = async () => {
     try {
-      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php'); //call
-      setData((prev) => response.data);
-      setLoad(false);
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      setData(response.data);
     } catch (err) {
-      setLoad(false);
-      console.log(err);
-
+      setError(err.message);
     }
-
   }
 
   useEffect(() => {
-    getData();
-
+    setTransition(getTodos);
   }, []);
 
-
-
-  if (load) {
-    return <h1>Loading......</h1>
+  if (pending) {
+    return <h1>Loading...</h1>
   }
 
+  if (err) {
+    return <h1>{err}</h1>
+  }
 
-
+  console.log(data);
 
   return (
     <div>
 
-
-      <h1>Meals Caategory</h1>
-
-      {data && data.categories?.map((cata) => {
-        return <div
-          onClick={() => nav(`/category-items/${cata.strCategory}`)}
-          className='cursor-pointer'
-          key={cata.idCategory}
-        >
-
-
-          <h1>{cata.strCategory}</h1>
-          <img src={cata.strCategoryThumb} alt="" />
-          <p className='line-clamp-3'>{cata.strCategoryDescription}</p>
-
+      {data && data.map((todo) => {
+        return <div key={todo.id}>
+          <h1>{todo.title}</h1>
         </div>
       })}
-
-
 
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from 'axios'
+// import React, { useEffect } from 'react'
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router';
+
+
+// export default function Home() {
+
+//   const nav = useNavigate();
+
+//   const [data, setData] = useState();
+//   const [load, setLoad] = useState(false);
+
+//   const getData = async () => {
+//     setLoad(true); /// calll
+//     try {
+//       const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php'); //call
+//       setData((prev) => response.data);
+//       setLoad(false);
+//     } catch (err) {
+//       setLoad(false);
+//       console.log(err);
+
+//     }
+
+//   }
+
+//   useEffect(() => {
+//     getData();
+
+//   }, []);
+
+
+
+//   if (load) {
+//     return <h1>Loading......</h1>
+//   }
+
+
+
+
+//   return (
+//     <div>
+
+
+//       <h1>Meals Caategory</h1>
+
+//       {data && data.categories?.map((cata) => {
+//         return <div
+//           onClick={() => nav(`/category-items/${cata.strCategory}`)}
+//           className='cursor-pointer'
+//           key={cata.idCategory}
+//         >
+
+
+//           <h1>{cata.strCategory}</h1>
+//           <img src={cata.strCategoryThumb} alt="" />
+//           <p className='line-clamp-3'>{cata.strCategoryDescription}</p>
+
+//         </div>
+//       })}
+
+
+
+//     </div>
+//   )
+// }
