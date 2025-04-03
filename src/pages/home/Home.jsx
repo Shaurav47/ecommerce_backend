2a3063205cf6180b431155b16c19bf36c38f2d90
Rabyn-Collab <babynshrestha64@gmail.com>
2025-04-03 08:50:23 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,90 @@
-import { Formik } from 'formik'
-import React from 'react'
+import axios from 'axios';
+import { Formik } from 'formik';
+import React, { useEffect, useState } from 'react'
+import Category from './Category';
+import SearchItem from './SearchItem';
 
 export default function Home() {
 
 
-  console.log('render');
+
+  const [data, setData] = useState();
+  const [load, setLoad] = useState(false);
+  const [search, setSearch] = useState(null);
+
+
+  const getData = async () => {
+    setLoad(true);
+    try {
+      if (search) {
+        const response = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php', {
+          params: {
+            s: search
+          }
+        });
+        setData((prev) => response.data);
+      } else {
+        const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+        setData((prev) => response.data);
+      }
+
+      setLoad(false);
+    } catch (err) {
+      setLoad(false);
+      console.log(err);
+    }
+
+  }
+  useEffect(() => {
+    getData();
+  }, [search]);
+
+  if (load) {
+    return <h1>Loading....</h1>
+  }
+
+
   return (
-    <div className='p-4'>
+    <div className='p-5'>
+
 
       <Formik
         initialValues={{
-          username: ''
+          query: ''
         }}
-        onSubmit={(val) => {
-          console.log(val);
 
+        onSubmit={(val) => {
+          setSearch(val.query);
         }}
+
       >
 
 
-        {({ handleChange, handleSubmit, values, touched }) => {
+        {({ handleChange, handleSubmit, values }) => {
 
-          return <form onSubmit={handleSubmit}>
-            <input
-              onChange={handleChange}
-              value={values.username}
-              className='border-2'
-              placeholder='username'
-              type="text"
-              name='username' />
+          return <form onSubmit={handleSubmit} className='space-y-2 flex gap-4 items-baseline'>
+            <div>
+              <input
+                className='border-2 border-gray-500 px-2 w-[300px] py-1'
+                value={values.query}
+                onChange={handleChange}
+                type="text" name='query'
+                placeholder='search for meals'
+              />
+            </div>
 
-            <button type='submit'>Submit</button>
+
+            <button className='bg-black text-white text-sm px-5 py-2 rounded-sm' type='submit'>Submit</button>
 
           </form>
-        }
+        }}
 
-        }
+
       </Formik>
+
+      {search ? <SearchItem data={data} /> : <Category data={data} />
+      }
+
 
 
     </div>
@@ -55,76 +103,3 @@ export default function Home() {
 
 
 
-
-
-
-
-// import axios from 'axios'
-// import React, { useEffect } from 'react'
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router';
-
-
-// export default function Home() {
-
-//   const nav = useNavigate();
-
-//   const [data, setData] = useState();
-//   const [load, setLoad] = useState(false);
-//   const [serach, setSearch] = useState(null);
-
-//   const getData = async () => {
-//     setLoad(true); /// calll
-//     try {
-//       const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php'); //call
-//       setData((prev) => response.data);
-//       setLoad(false);
-//     } catch (err) {
-//       setLoad(false);
-//       console.log(err);
-
-//     }
-
-//   }
-//   useEffect(() => {
-//     getData();
-
-//   }, []);
-
-
-
-//   if (load) {
-//     return <h1>Loading......</h1>
-//   }
-
-
-
-
-//   return (
-
-//     <div className='grid grid-cols-4 gap-5 p-5'>
-
-//       {data && data.categories.map((cata) => {
-//         return <div
-//           onClick={() => nav(`/category-items/${cata.strCategory}`)}
-//           className='cursor-pointer'
-//           key={cata.idCategory}
-//         >
-
-//           <h1>{cata.strCategory}</h1>
-//           <img src={cata.strCategoryThumb} alt="" />
-//           <p className='line-clamp-3'>{cata.strCategoryDescription}</p>
-
-//         </div>
-//       })}
-
-
-
-//     </div>
-
-
-
-
-
-//   )
-// }
