06b9fe6bab05c09ec491c6b7c36c398ca28ff84b
Rabyn-Collab <babynshrestha64@gmail.com>
2025-04-11 08:09:57 +0545

    changes

---
+++ src/pages/todo/taskSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+export const taskSlice = createSlice({
+  name: 'taskSlice',
+
+  initialState: {
+    tasks: [
+      { id: 1, title: 'some homework done', completed: false }
+    ]
+  },
+  reducers: {
+
+  }
+});
+
+
+export const { } = taskSlice.actions;
