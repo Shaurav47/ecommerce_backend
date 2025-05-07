62a680b8af26a236ff56254a86a6db95993c6283
Rabyn-Collab <babynshrestha64@gmail.com>
2025-05-07 09:10:38 +0545

    changes

--- middlewares/checkFile.js
+++ middlewares/checkFile.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const supportedTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
+const supportedTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/webp'];
 
 
 
